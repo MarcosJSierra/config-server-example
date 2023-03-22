4f59b87e91f6f0355d49cf4702b9d84ad257490d
Marcos Sierra <marcosjspac@gmail.com>
2023-03-22 16:13:32 -0600

    creacion de war

--- config-server/src/main/java/org/kurokami/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/org/kurokami/configserver/ConfigServerApplication.java
@@ -2,14 +2,22 @@ package org.kurokami.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class ConfigServerApplication {
+public class ConfigServerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		return application.sources(ConfigServerApplication.class);
 	}
 
 }
